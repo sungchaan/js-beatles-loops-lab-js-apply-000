4ea5ebd68057ef23d7033b4994ca1ff5e93e6f74
John Lee <jslee1992@gmail.com>
2019-02-26 03:56:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
   let musicAndInstru = [];
   for (let i = 0; i <= musicians.length-1; i++){
-    musicAndInstru.push(musicians[i] + ' plays ' instruments[i]);
+    musicAndInstru.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return musicAndInstru;
 }
