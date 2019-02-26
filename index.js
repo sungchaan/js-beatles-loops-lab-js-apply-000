acface19d9cb3157e962929eca7d3f68e0385060
John Lee <jslee1992@gmail.com>
2019-02-26 03:55:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let musicAndInstru = [];
+  for (let i = 0; i <= musicians.length-1; i++){
+    musicAndInstru.push(musicians[i] + ' plays ' instruments[i]);
+  }
+  return musicAndInstru;
+}
